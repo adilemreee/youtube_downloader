7eaea6c61297ea4daad89555acf889323e8d9ac5
adilemreee <adil.emre@hotmail.com>
2025-02-19 18:37:34 +0300

    yyee

--- app.py
+++ app.py
@@ -17,15 +17,11 @@ def youtube():
             ydl_opts = {
                 'format': 'best[ext=mp4]/best',
                 'noplaylist': True,
-                'http_headers': {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Referer': 'https://www.youtube.com/'
+
 }
 
-            }
+
+
 
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
@@ -54,16 +50,7 @@ def instagram():
             ydl_opts = {
                 'format': 'best[ext=mp4]/best',
                 'noplaylist': True,
-                'http_headers': {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Referer': 'https://www.youtube.com/'
 }
-
-            }
-
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
                 stream_url = info_dict.get('url', None)
